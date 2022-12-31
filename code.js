9f3f674e8970036527b75ca3692f614831d0416a
nosoyivan <plascencia925@gmail.com>
2022-12-30 16:15:35 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,51 @@
-    (function () {
+    //Race & Classes End
+    
+    var raceobject = {
+        Dwarves : 'Dwarves',
+        Elves : 'Elves',
+        Halflings : 'Halflings',
+        Humans : 'Humans',
+        Dragonborns : 'Dragonborns',
+        Gnomes : 'Gnomes',
+        Halfelves : 'Halfelves',
+        Halforcs : 'Halforcs',
+        Tieflings : 'Tieflings',
+    };
+    var select = document.getElementById("raceSelect");
+    for(index in raceobject) {
+        select.options[select.options.length] = new Option(raceobject[index], index);
+    }
+    
+    var claobject = {
+        Barbarian : 'Barbarian',
+        Bard : 'Bard',
+        Cleric : 'Cleric',
+        Druid : 'Druid',
+        Fighter : 'Fighter',
+        Monk : 'Monk',
+        Paladin : 'Paladin',
+        Ranger : 'Ranger',
+        Rogue : 'Rogue',
+        Sorcerer : 'Rogue',
+        Warlock : 'Warlock',
+        Wizard : 'Wizard',
+    };
+    var select = document.getElementById("claSelect");
+    for(index in claobject) {
+        select.options[select.options.length] = new Option(claobject[index], index);
+    }
+    //Race & Classes End
+    
+
+
+(function () {
     if (!console) {
         console = {};
     }
     var old = console.log;
     var logger = document.getElementById('outputLog');
     console.log = function (message) {
-        logger.innerHTML = '<pre>' + message + '</pre>';
+        logger.innerHTML = message ;
     }
 })();
 
@@ -91,46 +131,15 @@ console.log(localStorage.getItem('List'));
             var last = nodes[nodes.length- 1];
             document.querySelector('#cha').value =last.innerHTML;
         }
+        function CopyEX() {
+            var nodes = document.querySelectorAll('.copyex');
+            var last = nodes[nodes.length - 1];
+            document.querySelector('#task').value = last.innerHTML;
+
+        }
     //Copy End 
     
-    //Race & Classes End
-    
-    var raceobject = {
-        Dwarves : 'Dwarves',
-        Elves : 'Elves',
-        Halflings : 'Halflings',
-        Humans : 'Humans',
-        Dragonborns : 'Dragonborns',
-        Gnomes : 'Gnomes',
-        Halfelves : 'Halfelves',
-        Halforcs : 'Halforcs',
-        Tieflings : 'Tieflings',
-    };
-    var select = document.getElementById("raceSelect");
-    for(index in raceobject) {
-        select.options[select.options.length] = new Option(raceobject[index], index);
-    }
-    
-    var claobject = {
-        Barbarian : 'Barbarian',
-        Bard : 'Bard',
-        Cleric : 'Cleric',
-        Druid : 'Druid',
-        Fighter : 'Fighter',
-        Monk : 'Monk',
-        Paladin : 'Paladin',
-        Ranger : 'Ranger',
-        Rogue : 'Rogue',
-        Sorcerer : 'Rogue',
-        Warlock : 'Warlock',
-        Wizard : 'Wizard',
-    };
-    var select = document.getElementById("claSelect");
-    for(index in claobject) {
-        select.options[select.options.length] = new Option(claobject[index], index);
-    }
-    //Race & Classes End
-    
+
     //Database Start
         let mylist = [];
     
@@ -205,8 +214,146 @@ console.log(localStorage.getItem('List'));
             document.querySelector('#JSONinput').value =last.innerHTML;
         }
     //CopyJSON End    
+    
+    
 
-    //Connect
+
+
+
+
+
+            window.onload = () => {
+                const filter = document.querySelector("#filter");
+                // Filter Tasks event
+                filter.addEventListener("keyup", filterTask);
+
+                getTasks();
+                check();
+            }
+
+            // Get Tasks from LS
+            function getTasks() {
+                let tasks;
+
+                if (Object.keys(localStorage).length > 0) {
+                    // Loops over local storage keys
+                    Object.keys(localStorage).forEach(taskCat => {
+                        // Gets tasks for each task category
+                        tasks = JSON.parse(localStorage.getItem(taskCat));
+                        // Loop over each task
+                        tasks.forEach(task => {
+                            createTask(taskCat, task);
+                        });
+                    });
+                }
+            }
+
+            function check() {
+                const showMe = document.getElementById("showMe");
+                // Check if any tasks exist
+                showMe.style.display = Object.keys(localStorage).length > 0 ? "block" : "";
+            }
+
+            // Add Task
+            function addTask() {
+                const taskCat = document.getElementById("taskCat");
+                const taskInput = document.getElementById("task");
+                // Checks if valid task
+                if (taskCat.value == 0 || !taskInput.value) {
+                    console.log("Invalid inputs");
+                    return;
+                }
+
+                // Create task
+                createTask(taskCat.value, taskInput.value);
+                // Store in LS
+                addTaskToLocalStorage(taskCat.value, taskInput.value);
+                // Clear inputs
+                taskInput.value = "";
+                taskCat.value = 0;
+                check();
+            }
+
+            function createTask(taskCat, task) {
+                const taskList = document.getElementById("collection");
+                taskList.innerHTML += `
+    <li class="collection-item ${taskCat}">
+      ${task}
+      <button class="delete-item secondary-content" onclick="deleteTask(this, '${taskCat}', '${task}')">
+        DELETE
+      </button>
+    </li>
+  `;
+            }
+
+            // Store Tasks
+            function addTaskToLocalStorage(taskCat, task) {
+                const tasks = !localStorage.getItem(taskCat) ? [] : JSON.parse(localStorage.getItem(taskCat));
+
+                tasks.push(task);
+                localStorage.setItem(taskCat, JSON.stringify(tasks));
+            }
+
+            // Remove Tasks
+            function deleteTask(e, taskCat, task) {
+                if (confirm("Are You Sure?")) {
+                    e.parentElement.remove();
+                    deleteTaskFromLocalStorage(taskCat, task);
+                    check()
+                }
+            }
+
+            // Remove Tasks from LS
+            function deleteTaskFromLocalStorage(taskCat, task) {
+                const tasks = JSON.parse(localStorage.getItem(taskCat));
+                const index = tasks.indexOf(task);
+
+                tasks.splice(index, 1);
+
+                if (tasks.length > 0) {
+                    localStorage.setItem(taskCat, JSON.stringify(tasks))
+                } else {
+                    localStorage.removeItem(taskCat);
+                }
+            }
+
+            // Clear Tasks
+            function deleteTasks() {
+                const taskList = document.querySelector("#collection");
+                taskList.innerHTML = "";
+
+                localStorage.clear();
+                check();
+            }
+
+            // Filter Tasks
+            function filterTask(e) {
+                const text = e.target.value.toLowerCase();
+
+                document.querySelectorAll(".collection-item").forEach(function (task) {
+                    const item = task.firstChild.textContent;
+                    if (item.toLowerCase().indexOf(text) != -1) {
+                        task.style.display = "block";
+                    } else {
+                        task.style.display = "none";
+                    }
+                });
+            }
+
+            function filtertaskCat() {
+                const taskCatFilter = document.getElementById('taskCatFilter');
+                const taskListItems = document.querySelectorAll(".collection-item");
+
+                taskListItems.forEach(listItem => {
+                    if (taskCatFilter.value == 0 || listItem.classList.contains(taskCatFilter.value)) {
+                        listItem.style.display = "block";
+                    } else {
+                        listItem.style.display = "none";
+                    }
+                });
+            }
+            
+//Connect
 
     const { createClient } = supabase
     supabase = createClient('https://zsllqqspcgvfpyesjgwo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzbGxxcXNwY2d2ZnB5ZXNqZ3dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzIyNjQ2NDEsImV4cCI6MTk4Nzg0MDY0MX0.Uik4c0Ojpg_pj7seEjl73D2NvwgWbXHSDXx5b5bkcyY')
@@ -237,6 +384,3 @@ console.log(localStorage.getItem('List'));
         formInputs.forEach(element => element.value = '')
 
     })
-
-    //Connect
-
